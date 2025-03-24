19b8453049af73bafa5689a30a267715741e82c7
mtlccl <matheus.carvalho.lemes@hotmail.com>
2025-03-24 10:51:27 -0300

    spring cloud

---
+++ src/main/java/com/cloud/project/spring/back_end_spring_cloud/BackEndSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.project.spring.back_end_spring_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/project/spring/back_end_spring_cloud/BackEndSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.project.spring.back_end_spring_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
